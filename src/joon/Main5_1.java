e3e0e0a1dac67bd10ef8f860a759df2e244defdf
cosiw <cosiw@naver.com>
2022-11-26 19:11:44 +0900

    11.26 commit

---
+++ src/joon/Main5_1.java
@@ -0,0 +1,33 @@
+package joon;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main5_1 {
+
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		
+		String a = kb.next();
+
+		System.out.println(solution(a));
+	}
+	
+	public static String solution(String str) {
+		String answer = "YES";
+		
+		Stack<Character> stack = new Stack<>();
+		for(char x : str.toCharArray()) {
+			if(x=='(') stack.push(x);
+			else {
+				if(stack.isEmpty()) return "NO";
+				stack.pop(); //상단에 있는걸 꺼낸다.
+			}
+		}
+		
+		if(!stack.isEmpty()) return "NO";
+		
+		return answer;
+	}
+
+}
