e1cf2ebf7652d953f8e9e283ae38c98f7779639c
cosiw <cosiw@naver.com>
2022-12-05 19:30:45 +0900

    12.05 commit

---
+++ src/joon/Main6_3.java
@@ -0,0 +1,41 @@
+package joon;
+
+import java.util.Scanner;
+
+public class Main6_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Scanner kb = new Scanner(System.in);
+		
+		int n = kb.nextInt();
+		
+		int arr [] = new int[n];
+
+		for(int i=0; i<n; i++) {
+			arr[i] = kb.nextInt();
+		}
+		for(int x : solution(n,arr))
+		System.out.print(x+ " ");
+
+	}
+	
+	public static int [] solution(int n, int [] arr) {
+		//»ðÀÔ Á¤·Ä
+		int tmp = 0;
+		
+		for(int i =1; i<n; i++) {
+			tmp = arr[i]; int j=0;
+			for(j=i-1; j>=0; j--) {
+				if(arr[j] > tmp) {
+					arr[j+1] = arr[j];
+				} else break;
+				
+			}
+			arr[j+1] = tmp;
+		}
+		return arr;
+
+	}
+
+}
